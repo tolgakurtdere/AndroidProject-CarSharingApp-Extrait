09ddcbf45992eea0d27682abcc4f00cef05b665d
tolgahankurtdere <tolgahankurtdere@gmail.com>
2020-09-05 15:45:50 +0300

    clicking profile photo and go to mediaStore is done but nothing else (before starting bottom navigation bar)

--- app/src/main/java/com/tolgahankurtdere/extrait/ProfileActivity.java
+++ app/src/main/java/com/tolgahankurtdere/extrait/ProfileActivity.java
@@ -1,23 +1,50 @@
 package com.tolgahankurtdere.extrait;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.ImageDecoder;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
 import android.widget.CheckBox;
+import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+
+import java.io.IOException;
+import java.util.UUID;
 
 public class ProfileActivity extends AppCompatActivity {
 
     FirebaseAuth firebaseAuth;
     FirebaseFirestore firebaseFirestore;
+    FirebaseStorage firebaseStorage;
+    StorageReference storageReference;
 
+    ImageView profilePhotoImage;
     TextView nameText,surnameText,idNoText,emailText;
     CheckBox driverLicenseCheck;
+
+    Uri imageUriPP;
+    Bitmap selectedProfilePhoto;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,7 +55,10 @@ public class ProfileActivity extends AppCompatActivity {
 
         firebaseAuth = FirebaseAuth.getInstance();
         firebaseFirestore = FirebaseFirestore.getInstance();
+        firebaseStorage = FirebaseStorage.getInstance();
+        storageReference = firebaseStorage.getReference();
 
+        profilePhotoImage = findViewById(R.id.imageViewProfilePhoto);
         nameText = findViewById(R.id.textViewProfileName);
         surnameText = findViewById(R.id.textViewProfileSurname);
         idNoText = findViewById(R.id.textViewProfileIdNo);
@@ -48,4 +78,81 @@ public class ProfileActivity extends AppCompatActivity {
         });
 
     }
+
+    public void profilePhotoClicked(View view){
+
+        if(ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED){
+            ActivityCompat.requestPermissions(this,new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},1);
+        }
+        else{
+            Intent intentToGallery = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+            startActivityForResult(intentToGallery,2);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+
+        if(grantResults.length > 0){
+            if(requestCode == 1){
+                if(ContextCompat.checkSelfPermission(this,Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED){
+                    Intent intentToGallery = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                    startActivityForResult(intentToGallery,2);
+                }
+                else{
+                    //finish();
+                }
+            }
+        }
+
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+
+        if(requestCode == 2 && resultCode == RESULT_OK && data != null){
+            imageUriPP = data.getData();
+
+            try {
+                if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.P) { //Build.VERSION.SDK_INT >= 28
+                    ImageDecoder.Source source = ImageDecoder.createSource(this.getContentResolver(),imageUriPP);
+                    selectedProfilePhoto = ImageDecoder.decodeBitmap(source);
+                }
+                else{
+                    selectedProfilePhoto = MediaStore.Images.Media.getBitmap(this.getContentResolver(),imageUriPP);
+                }
+                profilePhotoImage.setImageBitmap(selectedProfilePhoto);
+
+            } catch (IOException e){
+                e.printStackTrace();
+            }
+        }
+        changePP();
+
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+
+    private void changePP(){
+        if(imageUriPP != null){
+            UUID uuid = UUID.randomUUID();
+
+            storageReference.child("ProfilePhotos/" + uuid).putFile(imageUriPP)
+                    .addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                        @Override
+                        public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                            Toast.makeText(ProfileActivity.this,"Profile photo is updated!",Toast.LENGTH_LONG).show();
+                        }
+                    })
+                    .addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception e) {
+                            Toast.makeText(ProfileActivity.this,e.getLocalizedMessage().toString(),Toast.LENGTH_LONG).show();
+                        }
+                    });
+
+        }
+
+    }
+
 }
